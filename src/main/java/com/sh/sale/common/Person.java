015c656703779777ff0ebfaad9404630786c66ea
developer1001 <yangpeng_188@sina.com>
2018-12-19 10:37:52 +0800

    Initial commit

---
+++ src/main/java/com/sh/sale/common/Person.java
@@ -0,0 +1,25 @@
+package com.sh.sale.common;
+
+public class Person {
+	private String name;
+	private String age;
+	
+	public Person() {
+		this.name = "default name";
+		this.age = "default age";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+
+}
